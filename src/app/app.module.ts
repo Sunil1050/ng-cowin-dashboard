f1cae9d90ed69fc41fb96143684545ca346519ce
sunilkumarc <sunilkumarc@zen3.co.uk>
2024-05-25 02:05:43 +0530

    fixed packages conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms'; // Import ReactiveFormsModule
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgApexchartsModule } from "ng-apexcharts";
 
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
